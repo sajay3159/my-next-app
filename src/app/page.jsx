9bccedf8cd2ad2523be3e3ddb18200b09352fa05
Ajay Sahani <sajay3159@gmail.com>
2025-10-19 23:49:17 +0530

    Layout task

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <h1>Welcome to the Products Store</h1>;
+  return (
+    <h1
+      style={{
+        padding: "1rem",
+        background: "#000000ff",
+        textAlign: "center",
+      }}
+    >
+      Welcome to the Products Store
+    </h1>
+  );
 }
